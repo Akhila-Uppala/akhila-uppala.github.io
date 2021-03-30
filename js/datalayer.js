96764e16f5c2d5a60f0df7876e9a240b28021707
Akhila-Uppala <akhilauppala.19@gmail.com>
2021-03-30 11:37:10 +0530

    data layer

--- js/datalayer.js
+++ js/datalayer.js
@@ -28,14 +28,15 @@
 					pageName: document.title,
 					pageURL : window.location.href,
 					siteSection :"example site section",
-					},
+					}
+				},
 			user: {
 				userID: "",
 				domain : window.location.hostname,
 				device : navigator.userAgent,
 				date : new Date().toDateString()
 				} 
-			}
+			
 		 }
 
 	
