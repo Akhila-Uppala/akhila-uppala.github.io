6b451c60f3231d15ea3a0c12cf3f999cce10c09c
Akhila-Uppala <akhilauppala.19@gmail.com>
2021-03-30 09:57:09 +0530

    data layer

---
+++ js/userID.js
@@ -0,0 +1,10 @@
+window.onload=function()
+        {
+    
+        let s=Visitor.getInstance("856F5BDE58C158A50A495D50@AdobeOrg").getMarketingCloudVisitorID()
+        digitalData.userID=s;
+        console.log(digitalData.page.pageName)
+        console.log(digitalData.userID)
+    
+    
+    }
